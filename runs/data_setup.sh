44f939b2ebda08fdec27d6be5a974e80284221a2
Alexander Mariona <amariona@mit.edu>
2026-06-29 11:44:32 -0400

    data script

---
+++ runs/data_setup.sh
@@ -0,0 +1,39 @@
+# Default intermediate artifacts directory is in ~/.cache/nanochat
+export OMP_NUM_THREADS=1
+export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat"
+mkdir -p $NANOCHAT_BASE_DIR
+
+# -----------------------------------------------------------------------------
+# Python venv setup with uv
+
+# install uv (if not already installed)
+command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
+# create a .venv local virtual environment (if it doesn't exist)
+[ -d ".venv" ] || uv venv
+# install the repo dependencies
+uv sync --extra gpu
+# activate venv so that `python` uses the project's venv instead of system python
+source .venv/bin/activate
+
+# -----------------------------------------------------------------------------
+# Tokenizer
+
+# Download the first ~2B characters of pretraining dataset
+# each data shard is ~250M chars
+# so we download 2e9 / 250e6 = 8 data shards at this point
+# each shard is ~100MB of text (compressed), so this is about ~800MB of data on disk
+# look at dev/repackage_data_reference.py for details on how this data was prepared
+python -m nanochat.dataset -n 8
+# Immediately also kick off downloading more shards in the background while tokenizer trains
+# Approximately 150 shards are needed for GPT-2 capability pretraining, add 20 for padding.
+# The maximum total number of shards available in the entire dataset is 6542.
+python -m nanochat.dataset -n 170 &
+DATASET_DOWNLOAD_PID=$!
+# train the tokenizer with vocab size 2**15 = 32768 on ~2B characters of data
+python -m scripts.tok_train
+# evaluate the tokenizer (report compression ratio etc.)
+python -m scripts.tok_eval
+
+# Base model (pretraining)
+echo "Waiting for dataset download to complete..."
+wait $DATASET_DOWNLOAD_PID
